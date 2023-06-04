5ea39112d7134c6c991c5f0d9836a4c7ea8965b5
mosmn <ithinkisee8@gmail.com>
2023-06-05 06:34:21 +0800

    added about and footer

--- src/index.js
+++ src/index.js
@@ -95,6 +95,24 @@ const header = () => {
 
 content.appendChild(header());
 
+const name = () => {
+    const name = document.createElement('h1');
+    name.classList.add('name');
+    name.textContent = 'Opulent Cuisine';
+
+    const subtitle = document.createElement('h2');
+    subtitle.classList.add('subtitle');
+    subtitle.textContent = 'Immerse yourself in an atmosphere of opulence and elegance as you savor our exquisite creations.';
+
+    const nameContainer = document.createElement('div');
+    nameContainer.classList.add('nameContainer');
+
+    nameContainer.appendChild(name);
+    nameContainer.appendChild(subtitle);
+
+    return nameContainer;
+}
+
 const img1Pic = () => {
     const img1Pic = document.createElement('img');
     img1Pic.src = img1;
@@ -112,9 +130,38 @@ const img2Pic = () => {
     return img2Pic;
 }
 
+content.appendChild(name());
 content.appendChild(img1Pic());
 content.appendChild(img2Pic());
     
+const about = () => {
+    const about = document.createElement('div');
+    about.classList.add('about');
 
+    const aboutText = document.createElement('p');
+    aboutText.classList.add('aboutText');
+    aboutText.textContent = 'Opulent Cuisine is a fine dining restaurant located in the heart of the city. We offer a wide variety of dishes, ranging from traditional to modern cuisine. Our chefs are experts in their field and will make sure that you have an unforgettable experience. Come visit us today!';
+
+    about.appendChild(aboutText);
+
+    return about;
+}
+
+content.appendChild(about());
+
+const footer = () => {
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+
+    const footerText = document.createElement('p');
+    footerText.classList.add('footerText');
+    footerText.textContent = '© 2021 Opulent Cuisine';
+
+    footer.appendChild(footerText);
+
+    return footer;
+}
+
+content.appendChild(footer());
 
 
